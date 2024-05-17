1c3f5a19894576e74c8d420268ddd1722fe2c25a
Max Wang <thewaxmango@gmail.com>
2024-05-17 11:37:42 -0400

    finish

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-yuh
+credit to landy8697 for collecting questions
+credit to eric wastl for css
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from json import load
+from random import choice
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    q = 'what is 2+2'
+    a = 0
+    s = 'placeholder source'
+    
+    with open('questions.json') as f:
+        d = choice(load(f))
+        q = d['question'].strip()
+        a = str(d['answer'])
+        s = d['source'].strip()
+    
+    return render_template("main.html", q=q, a=a, s=s)  
+
+app.debug = True
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
